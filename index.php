0e8f4226b1f36947d264514ba91bdc25b5659a18
RODOLFO LUIS PEREZ <a1420879@HTO.IFSP.EDU.BR>
2016-09-21 10:43:55 -0300

    alteração no mapa

--- index.php
+++ index.php
@@ -15,13 +15,8 @@
 	<script>
 	
 	function mapa(lt,lg){
-		if((lt==0) && (lg==0)){
-			var mymap = L.map('mapid').setView([-22.866270,-47.206627], 13);
-		}else{
-			//var mymap = L.map('mapid').setView([lt,lg], 17);
-			alert("var mymap = L.map('mapid').setView(["+lt+", "+lg+"], 17);");
-		}
-
+		var mymap = L.map('mapid').setView([-22.866270,-47.206627], 13);
+		
 		L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpandmbXliNDBjZWd2M2x6bDk3c2ZtOTkifQ._QA7i5Mpkd_m30IGElHziw', {
 			maxZoom: 18,
 			attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
@@ -51,11 +46,48 @@
 		L.marker([-22.890485, -47.199333], {icon: azulIcon}).bindPopup("Parque Santa Clara (CREAPE)").addTo(mymap);
 		L.marker([-22.868042, -47.218688], {icon: roxoIcon}).bindPopup("Parque Linear Chico Mendes").addTo(mymap);
 		L.marker([-22.860132, -47.219354], {icon: cianoIcon}).bindPopup("Antiga Estação Ferroviária de Hortolândia").addTo(mymap);
-	}		
+	}
+	
+    function mapa_p1(lt,lg){
+		//alert(lt +" / "+lg);
+		
+		var mymap = L.map('mapid').setView([-22.866270,-47.206627], 13);
+		
+		L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpandmbXliNDBjZWd2M2x6bDk3c2ZtOTkifQ._QA7i5Mpkd_m30IGElHziw', {
+			maxZoom: 18,
+			attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+				'<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+				'Imagery © <a href="http://mapbox.com">Mapbox</a>',
+			id: 'mapbox.streets'
+		}).addTo(mymap);
+		
+	
+		var LeafIcon = L.Icon.extend({
+			options: {
+				shadowUrl: 'img/iconesh.png',
+				iconSize:     [50, 50],
+				shadowSize:   [51, 51],
+				iconAnchor:   [4, 62],
+				shadowAnchor: [4, 62],
+				popupAnchor:  [-3, -76]
+			}
+		});
+
+		var tangeIcon = new LeafIcon({iconUrl: 'img/iconetange.png'}),
+			azulIcon = new LeafIcon({iconUrl: 'img/iconeazul.png'}),
+			roxoIcon = new LeafIcon({iconUrl: 'img/iconeroxo.png'});
+			cianoIcon = new LeafIcon({iconUrl: 'img/icone.png'});
+
+		L.marker([-22.896439, -47.169500], {icon: tangeIcon}).bindPopup("Parque Socioambiental Dorothy Stang").addTo(mymap);
+		L.marker([-22.890485, -47.199333], {icon: azulIcon}).bindPopup("Parque Santa Clara (CREAPE)").addTo(mymap);
+		L.marker([-22.868042, -47.218688], {icon: roxoIcon}).bindPopup("Parque Linear Chico Mendes").addTo(mymap);
+		L.marker([-22.860132, -47.219354], {icon: cianoIcon}).bindPopup("Antiga Estação Ferroviária de Hortolândia").addTo(mymap);
+	
+	}	
 	</script>
 <p>
 <table>
-<tr><td>Parque Socioambiental Dorothy Stang</td><td><button onclick="mapa(-22.896439, -47.169500);">Visitar</button></td></tr>
+<tr><td>Parque Socioambiental Dorothy Stang</td><td><button onclick="mapa_p1(-22.896439, -47.169500);">Visitar</button></td></tr>
 <tr><td>Parque Santa Clara (CREAPE)</td><td><button onclick="mapa(-22.890485, -47.199333);">Visitar</button></td></tr>
 <tr><td>Parque Linear Chico Mendes</td><td><button onclick="mapa(-22.868042, -47.218688);">Visitar</button></td></tr>
 <tr><td>Antiga Estação Ferroviária de Hortolândia</td><td><button onclick="mapa(-22.860132, -47.219354);">Visitar</button></td></tr>
