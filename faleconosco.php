765792b9152eb44b2cfff691a4ebcf97483a6fde
RODOLFO LUIS PEREZ <a1420879@HTO.IFSP.EDU.BR>
2016-09-21 11:40:08 -0300

    alteração no mapa

--- faleconosco.php
+++ faleconosco.php
@@ -3,7 +3,22 @@
 <meta charset="utf-8">
 </head>
 <body>
-<h2>Pagina de Contato</h2>
+<div align='left'><h2>Formulario de Contatos</h2></div>
+<form id="contact_form" action="#" method="POST" enctype="multipart/form-data">
+	<div class="row">
+		<label for="name">Seu Nome:</label><br />
+		<input id="name" class="input" name="name" type="text" value="" size="30" /><br />
+	</div>
+	<div class="row">
+		<label for="email">Seu Email:</label><br />
+		<input id="email" class="input" name="email" type="text" value="" size="30" /><br />
+	</div>
+	<div class="row">
+		<label for="message">Sua Message:</label><br />
+		<textarea id="message" class="input" name="message" rows="7" cols="30"></textarea><br />
+	</div><br>
+	<input id="submit_button" type="submit" value="Enviar" />
+</form>						
 <p><a href="#"onclick="javascript:history.go(-1);">Voltar</a>
 </body>
 </html>
